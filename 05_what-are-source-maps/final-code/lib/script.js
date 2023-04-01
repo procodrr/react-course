01ea73ed3427b63edfb93213e8a72d00605502fe
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2023-04-01 18:24:40 +0530

    Add files via upload

---
+++ 05_what-are-source-maps/final-code/lib/script.js
@@ -0,0 +1,29 @@
+const container = /*#__PURE__*/React.createElement("div", {
+  className: "container",
+  id: "container"
+}, /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement("p", null, "The library for web and native user interfaces"), /*#__PURE__*/React.createElement("img", {
+  src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+  style: {
+    width: 300,
+    backgroundColor: 'teal',
+    borderRadius: 8,
+    padding: 16
+  }
+})), /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement("form", null, /*#__PURE__*/React.createElement("div", {
+  className: "input-group"
+}, /*#__PURE__*/React.createElement("label", {
+  htmlFor: "username"
+}, "Username"), /*#__PURE__*/React.createElement("input", {
+  id: "username"
+})), /*#__PURE__*/React.createElement("div", {
+  className: "input-group"
+}, /*#__PURE__*/React.createElement("label", {
+  htmlFor: "password"
+}, "Password"), /*#__PURE__*/React.createElement("input", {
+  id: "password",
+  type: "password"
+})))));
+console.log(container);
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(container);
+//# sourceMappingURL=script.js.map
---
+++ 05_what-are-source-maps/final-code/script.js
@@ -0,0 +1,34 @@
+const container = (
+  <div className="container" id="container">
+    <section>
+      <p>The library for web and native user interfaces</p>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        style={{
+          width: 300,
+          backgroundColor: 'teal',
+          borderRadius: 8,
+          padding: 16,
+        }}
+      />
+    </section>
+    <section>
+      <form>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input id="username" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" />
+        </div>
+      </form>
+    </section>
+  </div>
+)
+
+console.log(container);
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(container)
