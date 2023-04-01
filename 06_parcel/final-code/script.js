1ff8a3a6157cdf70cdbd812a2074825d27d04b71
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2023-04-01 18:29:19 +0530

    Delete 06_parcel directory

--- 06_parcel/final-code/script.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-
-const h1 = <h1>Hello World</h1>
-
-const root = createRoot(document.getElementById('root'))
-
-root.render(h1)
-console.log('Hello world!!!')
