452bb33a101d26ebdca6ae806f25e7b6e509118f
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2023-04-04 10:06:49 +0530

    Add files via upload

---
+++ 09_introduction-to-react-components/final-code/script.js
@@ -0,0 +1,56 @@
+import { createRoot } from 'react-dom/client'
+import './style.css'
+import React from 'react'
+
+function Card(props) {
+  const { title, image, brand, price } = props
+  return (
+    <div className="card">
+      <img src={image} alt="iphone" />
+      <div className="card-content">
+        <h3>{title}</h3>
+        <p>{brand}</p>
+        <p>
+          <b>${price}</b>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root'))
+
+console.log('Hello world!!!')
+
+fetch('https://dummyjson.com/products')
+  .then((res) => res.json())
+  .then((data) => {
+    root.render(
+      <div className="container">
+        {data.products.map((product) => {
+          return (
+            <Card
+              key={product.id}
+              title={product.title}
+              brand={product.brand}
+              price={product.price}
+              image={product.thumbnail}
+            />
+          )
+        })}
+      </div>
+    )
+  })
+
+// root.render({
+//   $$typeof: Symbol.for('react.element'),
+//   type: Card,
+//   ref: null,
+//   props: {
+//     title: 'iPhone 13',
+//     image: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+//     brand: 'Apple',
+//     price: 899,
+//     key: 12
+//   }
+// })
---
+++ 10_how-use-images-in-react/final-code/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ imageUrl }) => {
+  return (
+    <button title="hello">
+      <img src={imageUrl} alt="button" />
+    </button>
+  )
+}
+
+export default Button
