87faf5f2c2e17b8f1b1a00165ada7bf714930879
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2023-04-29 19:24:17 +0530

    Add files via upload

---
+++ 26_pass-data-using-react-router/starter-code/components/CountryCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function CountryCard({ name, flag, population, region, capital }) {
+  return (
+    <Link className="country-card" to={`/${name}`}>
+      <img src={flag} alt={name + ' Flag'} />
+      <div className="card-text">
+        <h3 className="card-title">{name}</h3>
+        <p>
+          <b>Population: </b>
+          {population.toLocaleString('en-IN')}
+        </p>
+        <p>
+          <b>Region: </b>{region}
+        </p>
+        <p>
+          <b>Capital: </b>{capital}
+        </p>
+      </div>
+    </Link>
+  )
+}
